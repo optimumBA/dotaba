ec959a45b241c5a761f3ea6343b4cff64f176f4d
Almir Sarajčić <TB_Almir@hotmail.com>
2013-01-15 16:33:17 +0100

    Add minion module

--- application/config/modules.php
+++ application/config/modules.php
@@ -3,6 +3,7 @@
 return array(
 	'database'   => MODPATH.'database',
 	'email'      => MODPATH.'email',
+	'minion'     => MODPATH.'minion',
 	'orm'        => MODPATH.'orm',
 	'pagination' => MODPATH.'pagination'
 );
