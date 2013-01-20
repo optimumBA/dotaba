e028ae37c0f6628eac41cda392c67db267fb1f9e
Almir Sarajčić <TB_Almir@hotmail.com>
2013-01-20 10:01:59 +0100

    Add users:cacheavatars task
    Add two static methods to Steam class: get_avatar() and cache_avatar().
    Steam::get_avatar() checks if the avatar is already cached. If it is then it returns location on the server. If it isn't, then it returns URL to external resource.
    Steam::cache_avatar() saves avatar on server.

--- application/config/modules.php
+++ application/config/modules.php
@@ -3,6 +3,7 @@
 return array(
 	'database'   => MODPATH.'database',
 	'email'      => MODPATH.'email',
+	'image'      => MODPATH.'image',
 	'minion'     => MODPATH.'minion',
 	'orm'        => MODPATH.'orm',
 	'pagination' => MODPATH.'pagination'
