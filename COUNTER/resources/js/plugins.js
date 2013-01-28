2cc0bcd231d7bae7f7c502e6ffc8db79dabe6a3a
Amar Kasapović <a.ke@live.com>
2013-01-29 00:13:17 +0100

    Deleted counter.

--- COUNTER/resources/js/plugins.js
+++
@@ -1,1 +0,0 @@
-$("input.placeholder").each(function(){var b=$(this),a=$(this).val();$(this).focus(function(){if(b.val()==a){$(this).val("")}});$(this).blur(function(){if(b.val()==""){$(this).val(a)}})});
