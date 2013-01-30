21e310611a2d3cfa977bc5698aabfd1ed117a0ba
Almir Sarajčić <TB_Almir@hotmail.com>
2013-01-30 10:20:35 +0100

    Improve routes

--- application/routes.php
+++ application/routes.php
@@ -1,9 +1,15 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-Route::set('news', 'novosti(/<id>-<title>(/<action>))', array('title' => '[a-zA-Z0-9_-]+'))
+Route::set('news', 'novosti(/<page>)', array('page' => '\d+'))
 	->defaults(array(
 		'controller' => 'news',
 		'action'     => 'index',
+	));
+
+Route::set('article', 'novosti/<id>-<title>(/<action>)', array('title' => '[a-zA-Z0-9_-]+'))
+	->defaults(array(
+		'controller' => 'news',
+		'action'     => 'view',
 	));
 
 Route::set('users', '<action>', array('action' => 'prijava|odjava'))
