734baed9f1208fa24d1224a63c4a4ca3df5b1ba1
Amar Kasapović <a.ke@live.com>
2013-02-05 18:47:03 +0100

    UI, page layouts add.

---
+++ application/views/pages/login.php
@@ -0,0 +1,89 @@
+<div class="clear"></div>
+    <!-- Banner Start -->
+    <div id="sub-banner">
+    	<div class="in">
+        	<a href="#"><img src="/assets/images/UI/Steam_banner.png" alt="" /></a>
+        </div>
+    </div>
+    <!-- Banner End -->
+    <!-- Content Section Start -->
+    <div id="content-sec">
+    	<div class="inner">
+        	<!-- Columns Section Start -->
+            <div class="columns-sec twocol">
+            	<!-- Column Three Start -->
+                <div class="col3">
+                	<div class="contact">
+                    	<h1 class="heading colr">Steam autentifikacija</h1>
+                        <!-- Contact Us Start -->
+                        <div class="contact-page">
+                        	<div class="cont-sec">
+                               
+                                <div class="desc">
+                                    <h3 class="colr">Provjera</h3>
+                                    <p>
+                                        
+                                        
+                                       Da bi se autentifikovali na stranicu i dobili željene podatke, potrebno je kliknuti na duge Steam autentifikacije, koja će vas nakon toga preusmjeriti na službenu stranicu Steam-a.
+                                       <br /><br />Klikom na dugme prijave se slažete sa našim pravima i uvijetima, te pravilima privatnosti.
+                                       
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="inquiry">
+                            	<h1 class="heading colr">Potvrda</h1>
+                               
+                               <a href="/prijava">
+                               <div class="SteamSITSLarge"></div>
+                               </a>
+                            
+                            </div>
+                        </div>
+                        <div class="clear"></div>
+                        <!-- Post Detail End -->
+                    </div>
+                </div>
+                <!-- Column One End -->
+                <!-- Column One Start -->
+                <div class="col1 hidemobile">
+                	<!-- Text Widget Start -->
+                	<div class="widget text-widget">
+                    	<h1 class="heading colr">Info</h1>
+                        <p class="bold">
+                            SSL autentifikacija preko Steam-a
+                         
+                        </p>
+                        <p>
+                        	SSL<br />
+                            Domena: dota.ba<br />
+                            OpenID<br />
+                            WebAPI<br />
+                        </p>
+                        
+                        <p>
+                        	<span class="colr">Podrška</span><br />
+                            
+							podrska@dota.ba<br />
+                        </p>
+                        
+                         <p>
+                        	<span class="colr"><a href="/site/privacy">Privacy</a></span>
+                            
+						
+                        </p>
+                         <p>
+                        	<span class="colr"><a href="/site/terms">Terms</a></span>
+                            
+							
+                        </p>
+                    </div>
+                    <!-- Text Widget End -->
+                    
+                </div>
+                <!-- Column One End -->
+            </div>
+            <!-- Columns Section End -->
+        </div>
+    </div>
+    <!-- Content Section End -->
+    <div class="clear"></div>
