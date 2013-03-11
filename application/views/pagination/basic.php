c6e456f53b9d74a959ebfed77ce46785b30b63cb
Almir Sarajčić <TB_Almir@hotmail.com>
2013-03-11 09:31:31 +0100

    Fix pagination module
    Use GitHub repository almirsarajcic/pagination.git instead of shadowhand/pagination.git until pull request is merged.
    Add path to cache module in .gitmodules.

--- application/views/pagination/basic.php
+++ application/views/pagination/basic.php
@@ -1,41 +1,35 @@
 <div class="paging">
-		<ul>
-
-	<?php if ($first_page !== FALSE): ?>
-    	<li><a href="<?php echo HTML::chars($page->url($first_page)) ?>"><?php echo __('Prva') ?></a></li>
-    <?php else: ?>
-    	<li><a href="#"><?php echo __('Prva') ?></a></li>
-    <?php endif ?>
-
-	<?php if ($previous_page !== FALSE): ?>
-		<li><a href="<?php echo HTML::chars($page->url($previous_page)) ?>" rel="prev"><?php echo __('Prethodna') ?></a></li>
-	<?php else: ?>
-		<li><a href="#"><?php echo __('Prethodna') ?></a></li>
-	<?php endif ?>
-
-	<?php for ($i = 1; $i <= $total_pages; $i++): ?>
-
-		<?php if ($i == $current_page): ?>
-			<li><a href="#" class="active"><?php echo $i ?></a></li>
+	<ul>
+		<?php if ($first_page !== FALSE): ?>
+			<li><a href="<?php echo HTML::chars($page->url($first_page)); ?>">Prva</a></li>
 		<?php else: ?>
-			<li><a href="<?php echo HTML::chars($page->url($i)) ?>"><?php echo $i ?></a></li>
+			<li><a href="#">Prva</a></li>
 		<?php endif ?>
 
-	<?php endfor ?>
+		<?php if ($previous_page !== FALSE): ?>
+			<li><a href="<?php echo HTML::chars($page->url($previous_page)); ?>" rel="prev">Prethodna</a></li>
+		<?php else: ?>
+			<li><a href="#">Prethodna</a></li>
+		<?php endif ?>
 
-	<?php if ($next_page !== FALSE): ?>
-		<li><a href="<?php echo HTML::chars($page->url($next_page)) ?>" rel="next"><?php echo __('Sljedeća') ?></a></li>
-	<?php else: ?>
-		<li><a href="#"><?php echo __('Sljedeća') ?></a></li>
-	<?php endif ?>
-    
-    	<?php if ($last_page !== FALSE): ?>
-    	<li><a href="<?php echo HTML::chars($page->url($last_page)) ?>"><?php echo __('Zadnja') ?></a></li>
-    <?php else: ?>
-    	<li><a href="#"><?php echo __('Zadnja') ?></a></li>
-    <?php endif ?>
-    
-    
-		</ul>
-</div>
-<!-- .pagination -->
+		<?php for ($i = 1; $i <= $total_pages; $i++): ?>
+			<?php if ($i == $current_page): ?>
+				<li><a href="#" class="active"><?php echo $i; ?></a></li>
+			<?php else: ?>
+				<li><a href="<?php echo HTML::chars($page->url($i)); ?>"><?php echo $i; ?></a></li>
+			<?php endif ?>
+		<?php endfor ?>
+
+		<?php if ($next_page !== FALSE): ?>
+			<li><a href="<?php echo HTML::chars($page->url($next_page)); ?>" rel="next">Sljedeća</a></li>
+		<?php else: ?>
+			<li><a href="#">Sljedeća</a></li>
+		<?php endif ?>
+
+		<?php if ($last_page !== FALSE): ?>
+			<li><a href="<?php echo HTML::chars($page->url($last_page)); ?>">Zadnja</a></li>
+		<?php else: ?>
+			<li><a href="#">Zadnja</a></li>
+		<?php endif ?>
+	</ul>
+</div><!-- .pagination -->
