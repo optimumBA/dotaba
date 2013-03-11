a689e38546c24b2b28e7efe0002b09775421a66f
Amar Kasapović <a.ke@live.com>
2013-03-11 23:36:41 +0100

    Improve pagination

--- application/views/pagination/basic.php
+++ application/views/pagination/basic.php
@@ -2,14 +2,14 @@
 	<ul>
 		<?php if ($first_page !== FALSE): ?>
 			<li><a href="<?php echo HTML::chars($page->url($first_page)); ?>">Prva</a></li>
-		<?php else: ?>
-			<li><a href="#">Prva</a></li>
+		<?php elseif ($first_page == TRUE): ?>
+			<li><a href="#prva">Prva</a></li>
 		<?php endif ?>
 
 		<?php if ($previous_page !== FALSE): ?>
 			<li><a href="<?php echo HTML::chars($page->url($previous_page)); ?>" rel="prev">Prethodna</a></li>
-		<?php else: ?>
-			<li><a href="#">Prethodna</a></li>
+		<?php elseif ($previous_page == TRUE): ?>
+			<li><a href="#prethodna">Prethodna</a></li>
 		<?php endif ?>
 
 		<?php for ($i = 1; $i <= $total_pages; $i++): ?>
@@ -22,14 +22,14 @@
 
 		<?php if ($next_page !== FALSE): ?>
 			<li><a href="<?php echo HTML::chars($page->url($next_page)); ?>" rel="next">Sljedeća</a></li>
-		<?php else: ?>
-			<li><a href="#">Sljedeća</a></li>
+		<?php elseif($next_page == TRUE): ?>
+			<li><a href="#sljedeca">Sljedeća</a></li>
 		<?php endif ?>
 
 		<?php if ($last_page !== FALSE): ?>
 			<li><a href="<?php echo HTML::chars($page->url($last_page)); ?>">Zadnja</a></li>
-		<?php else: ?>
-			<li><a href="#">Zadnja</a></li>
+		<?php elseif ($last_page == TRUE): ?>
+			<li><a href="#zadnja">Zadnja</a></li>
 		<?php endif ?>
 	</ul>
 </div><!-- .pagination -->
