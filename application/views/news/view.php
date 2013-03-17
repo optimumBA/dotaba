58ff4cdd560a78abe96c7c7f25a10871f2fd3f8f
Almir Sarajčić <TB_Almir@hotmail.com>
2013-03-17 15:27:37 +0100

    Refactor comments' view code

--- application/views/news/view.php
+++ application/views/news/view.php
@@ -46,61 +46,7 @@
                         <div class="clear"></div>
                         <!-- Post Detail End -->
                         
-     <!-- Comments Start -->
-      <div class="comments">
-          <h1 class="heading colr">Komentari</h1>
-		  
-		  		 <?php if (!Steam::logged_in()): ?>
-                 <div class="alert alert-error">Moraš biti prijavljen/a kako bi ostavio/la komentar. Prijavi se <a href="/provjera">ovdje</a></div>
-				 <?php endif; ?>
-                 <div class="clear"></div>
-                 
-				 <?php if (count($comments)): ?>
-                       <ul>
-                        <?php foreach ($comments as $comment): ?>
-                            <li>
-                                <div class="avatar">
-			<?php echo HTML::image(Media_Remote_Avatar::get($comment->user->id, $comment->user->avatar), array('alt' => $comment->user->username, 'width' => 60, 'height' => 60)); ?></div>
-                 
-				 					<div class="desc">
-				 		 			<h5><?php echo HTML::anchor('igraci/'.$comment->user->accountid, $comment->user->username); ?></h5>
-                                    <p class="ago"> <?php echo Date::formatted_time($comment->created_at); ?></p>
-                                    <p class="txt"><?php echo HTML::parse_bbcode($comment->body); ?> </p>
-                                    <div class="clear"></div>    
-                                   
-                                   
-                                  </div>
-                               </li>
-                                           
-                                        
-                                    <?php endforeach ?>
-                                </ul>
-                            <?php else: ?>
-                                <div class="alert alert-info">Trenutno nema komentara.</div>
-                                
-                            <?php endif; ?>
-                        </div>
-                        <!-- Comments End -->
-						<?php if (Steam::logged_in()): ?>
-                            <!-- Leave a Reply Start -->
-                            <div class="leavereply">
-                                <h1 class="heading colr">Dodaj komentar</h1>
-                                <?php echo Form::open('komentari/dodaj', array('class' => 'forms')); ?>
-                                    <ul>
-                                        <li>
-                                            <?php echo Form::textarea('body', '', array('placeholder' => 'Tekst')); ?>
-                                        </li>
-                                        <li>
-                                            <?php echo Form::hidden('object_id', $article->id); ?>
-                                            <?php echo Form::hidden('object_type', 'News'); ?>
-                                            <?php echo Form::hidden('csrf', Security::token()); ?>
-                                            <?php echo Form::submit(NULL, 'Pošalji'); ?>
-                                        </li>
-                                    </ul>
-                                <?php echo Form::close(); ?>
-                            </div>
-                            <!-- Leave a Reply End -->
-                        <?php endif ?>
+     <?php echo View::factory('comments', array('comments' => $comments, 'object_id' => $article->id, 'object_type' => 'News')); ?>
                     </div>
                
                 </div>
