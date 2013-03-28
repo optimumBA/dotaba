aa36bdb032cba52ed36defba3b81bfd457b60a61
Almir Sarajčić <TB_Almir@hotmail.com>
2013-03-28 18:35:12 +0100

    Fix minor issues with path
    Use full path for 'data' and 'media' directories.

--- application/config/steam.php
+++ application/config/steam.php
@@ -5,6 +5,6 @@ return array(
 	'api_key'      => 'A754613E4D3F7E8268402052CD2AF3A7',
 	'provider'     => 'http://steamcommunity.com/openid',
 	'app_id'       => 570,
-	'matches_path' => 'data'.DIRECTORY_SEPARATOR.'matches',
-	'items_path'   => 'data'.DIRECTORY_SEPARATOR.'items.txt',
+	'matches_path' => APPPATH.'..'.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'matches',
+	'items_path'   => APPPATH.'..'.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'items.txt',
 );
