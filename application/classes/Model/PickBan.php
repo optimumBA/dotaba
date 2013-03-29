a64c34c687440350fda2c0db0996056b2571db59
Almir Sarajčić <TB_Almir@hotmail.com>
2013-03-29 17:29:50 +0100

    Fix class name issue

--- application/classes/Model/PickBan.php
+++ application/classes/Model/PickBan.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Model_PickBan extends ORM {
+class Model_Pickban extends ORM {
 
 	protected $_belongs_to = array(
 		'hero'  => array(),
