1a81e00c05e7f02ae32eca38f022db8bc6babeea
Amar Kasapović <a.ke@live.com>
2013-04-08 22:04:06 +0200

    Fix minor issue with stream URL

--- application/views/vods/videos/index.php
+++ application/views/vods/videos/index.php
@@ -6,7 +6,7 @@
             <div class="col4">
             	<div class="gallery-head">
                 	<h1 class="colr">Snimci</h1>
-                    <nav id="filter"><a class="" href="/">Početna</a><a class="" href="/streamovi">Streamovi</a><a class="active" href="#">Snimci</a></nav>
+                    <nav id="filter"><a class="" href="/">Početna</a><a class="" href="/vods/streamovi">Streamovi</a><a class="active" href="#">Snimci</a></nav>
                     <div class="clear"></div>
                 </div>
                 <section id="gal-container">
